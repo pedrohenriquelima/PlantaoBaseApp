92cb1ca73d39d72208a249c7cfdc7aa07be92230
pedrohenriquelima <pedroolima95@gmail.com>
2018-01-25 11:16:15 -0200

    Delete firebase.credentials.1.ts

--- src/app/firebase.credentials.1.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyDGkBcL5GnRrb8itpEFOE4TA9GcziA0aME",
-    authDomain: "plantaopims.firebaseapp.com",
-    databaseURL: "https://plantaopims.firebaseio.com",
-    projectId: "plantaopims",
-    storageBucket: "plantaopims.appspot.com",
-    messagingSenderId: "103914363728"
-};
